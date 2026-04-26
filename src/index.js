286974024c508b5650b4b57c10afb8a6ac1487bd
dhruvil <dhruvilkatharotiya@gmail.com>
2026-04-26 12:04:20 +0530

    db connected through db folder

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
 import dotenv from "dotenv"
 import mongoose from "mongoose";
 import { DB_NAME } from "./constants.js";  //.js is necessary
-//import connectDB from "./db/index.js";  //.js is necessary
-//connectDB() is not used because we are connecting to the database directly in 
-//      the IIFE below. You can choose to use connectDB() if you prefer a cleaner 
-//      separation of concerns, but for simplicity, I've included the connection 
-//      logic directly in the IIFE.
+import connectDB from "./db/index.js";  //.js is necessary
+
 dotenv.config({
     path: "./.env"
 })
-import express from "express";
-const app = express();
 
-;(async () => { // ()() is an IIFE (Immediately Invoked Function Expression) to use async/await at the top level
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        app.on("error", (error)=>{
-            console.log("ERROR in app.on" , error)
-            throw error
-        })
-        app.listen(process.env.PORT, ()=>{
-            console.log(`Server is running on port ${process.env.PORT}`)
-        })
-    } catch (error) {
-        console.log("Error in connecting to database", error);
-        throw error;
-    }
-})();
+connectDB() //is not used because we are connecting to the database directly in 
+     //the IIFE below. You can choose to use connectDB() if you prefer a cleaner 
+     //separation of concerns, but for simplicity, I've included the connection 
+     //logic directly in the IIFE.
+
+// import express from "express";
+// const app = express();
+
+// ;(async () => { // ()() is an IIFE (Immediately Invoked Function Expression) to use async/await at the top level
+//     try {
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error", (error)=>{
+//             console.log("ERROR in app.on" , error)
+//             throw error
+//         })
+//         app.listen(process.env.PORT, ()=>{
+//             console.log(`Server is running on port ${process.env.PORT}`)   // or ("..",process.env.PORT) is also fine.
+//         })
+//     } catch (error) {
+//         console.log("Error in connecting to database", error);
+//         throw error;
+//     }
+// })();
